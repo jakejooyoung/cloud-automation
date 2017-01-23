b791fa7bb390d574c87b3c8d5bd42f20f82933f4
Jake Kim <jake@npgains.com>
2017-01-22 16:30:22 -0800

    Patch Go web server's user data.
    Previously the biggest problem was how the paths would change, while in sudo and ubuntu.
    Initially I tried to see if there's a way to reliably track the changes in relative path.
    But it turned out the problem was that the directory created during userdata stage is run
    as sudo so when user is logged in as ubuntu he doesn't have permissions to the work/ directory.
    So all I had to do is change the ownership of the directory, and specify the $GOPATH.
    One thing I also had to do though is export $GOPATH and $PATH in both the userdata script and also
    cat it into profile.d/xxx.sh file so that when the process finishes, bashrc will set these env variables.
    
    TO-DO:
    
    Pull from bitbucket or something on launch.
    Better way to sync s3 userdata files.
    Configure webserver to interact with database.
    Write database for basic user and order placements.
    Hook it up with Stripe's payment API.
    Clean up UI.

--- ConfigFiles/UserData/go.sh
+++ ConfigFiles/UserData/go.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 set -x
-export DEBIAN_FRONTEND=noninteractive
+export DEBIAN_FRONTEND=noninteractive	
 
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 echo BEGIN
 date '+%Y-%m-%d %H:%M:%S'
 echo END
 
-add-apt-repository ppa:ubuntu-lxc/lxd-stable
+sudo apt-add-repository ppa:ubuntu-lxc/lxd-stable
 apt-get update
-apt-get install -y golang
-	
-GOPATH=home/ubuntu/work
-PATH=$PATH:$GOPATH/bin
-mkdir -p $GOPATH/src/bitbucket.org/gainsresearch/goapp
-
-cat > home/ubuntu/.bash_aliases <<EOL
-export GOPATH=$HOME/work/
-export PATH=$PATH:$GOPATH/bin
-export GOBIN=$HOME/work/bin
+sudo apt-get -y upgrade
+sudo apt-get -y install golang
+cat > /etc/profile.d/two<<EOL
+$HOME
+tell me where is home 
 EOL
-exit 0
- 
-" > /etc/rc.local
- 
+
+export GOPATH=/home/ubuntu/work
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+cat > /etc/profile.d/setgoenv.sh <<EOL
+export GOPATH=/home/ubuntu/work
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+EOL
+
+MAINGO=$GOPATH/src/bitbucket.org/gainsresearch/goserver
+mkdir -p $MAINGO/tmpl
+cat > /home/ubuntu/work/src/bitbucket.org/gainsresearch/goserver/tmpl/edit.html <<EOL
+<h1> EDIT NOW </h1>
+EOL
+cat > /home/ubuntu/work/src/bitbucket.org/gainsresearch/goserver/tmpl/view.html <<EOL
+<h1> EDIT TOMORROW </h1>
+EOL
+chown -R ubuntu:staff $GOPATH
+go get github.com/lib/pq
 reboot 0
