24a7da62ab488c5b9fcbcf20d2589d30e6563f94
Jake Kim <jake@npgains.com>
2017-03-07 05:01:24 -0800

    Add script that launches go server EC2. Also injects presigned url into UserData for go to fetch bitbucket access key from a private S3.

--- ConfigFiles/UserData/go.sh
+++ ConfigFiles/UserData/go.sh
@@ -7,6 +7,10 @@ echo BEGIN
 date '+%Y-%m-%d %H:%M:%S'
 echo END
 
+keyurl="PRESIGNED_URL"
+curl -o /home/ubuntu/key $keyurl
+cat /home/ubuntu/key
+
 sudo apt-add-repository ppa:ubuntu-lxc/lxd-stable
 apt-get update
 sudo apt-get -y upgrade
@@ -22,14 +26,12 @@ export GOPATH=/home/ubuntu/work
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 EOL
 
-MAINGO=$GOPATH/src/bitbucket.org/gainsresearch/goserver
-mkdir -p $MAINGO/tmpl
-cat > /home/ubuntu/work/src/bitbucket.org/gainsresearch/goserver/tmpl/edit.html <<EOL
-<h1> EDIT NOW </h1>
-EOL
-cat > /home/ubuntu/work/src/bitbucket.org/gainsresearch/goserver/tmpl/view.html <<EOL
-<h1> EDIT TOMORROW </h1>
-EOL
+MYGO=$GOPATH/src/bitbucket.org/gainsresearch/goserver
+
+cd $MYGO 
+git clone git@bitbucket.org:gainsresearch/go.git .
+git co -b gostructure
+
 chown -R ubuntu:staff $GOPATH
 go get github.com/lib/pq
 reboot 0
