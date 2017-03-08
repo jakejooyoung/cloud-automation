baac141e491877d662e7853b3727504a7bc2ba8e
Jake Kim <jake@npgains.com>
2017-03-07 20:48:48 -0800

    Implemented core components of launch_go.sh.
    1. Request presigned url for bitbucket repo access key from S3 bucket.
    2. Get userdata and replace placeholder variable with fetched url.
    3. Launch EC2 instance for go server and install dependancies.
    4. Set bitbucket as one of the known_hosts to enable git clone.
    5. Set up GOPATH and working directory.
    6. Git clone from bitbucket - ../gainsresearch/go
    
    TO-DO:
    - Merge master with gostructure.
    - Accept args.
    - Refactor so launch_go and launch_nginx can work together.

--- ConfigFiles/UserData/go.sh
+++ ConfigFiles/UserData/go.sh
@@ -7,31 +7,54 @@ echo BEGIN
 date '+%Y-%m-%d %H:%M:%S'
 echo END
 
+# Placeholder variable exchanged for presigned, time-limit enforced access to npgains.keys/ S3 bucket.
 keyurl="PRESIGNED_URL"
-curl -o /home/ubuntu/key $keyurl
-cat /home/ubuntu/key
 
 sudo apt-add-repository ppa:ubuntu-lxc/lxd-stable
 apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install golang
 cat > /etc/profile.d/debug<<EOL
-$HOME
+	$HOME
 EOL
 
+# Prepare GOPATH and working directory 
 export GOPATH=/home/ubuntu/work
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 cat > /etc/profile.d/setgoenv.sh <<EOL
-export GOPATH=/home/ubuntu/work
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+	export GOPATH=/home/ubuntu/work
+	export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 EOL
+MYGO=$GOPATH/src/bitbucket.org/gainsresearch/go
+mkdir -p $MYGO
 
-MYGO=$GOPATH/src/bitbucket.org/gainsresearch/goserver
+# # Configure ssh identity
+# cat > /home/ubuntu/.ssh/config <<EOL
+# Host bitbucket.org
+# IdentityFile /home/ubuntu/.ssh/repo_rsa
+# EOL
 
-cd $MYGO 
+# Would save bitbucket host fingerprint to ubuntu accessible location.
+# ssh-keyscan -t rsa bitbucket.org >> /home/ubuntu/.ssh/known_hosts
+# Instead we save bitbucket host fingerprint to root accessible location.
+ssh-keyscan -t rsa bitbucket.org >> ~/.ssh/known_hosts
+# Start the ssh-agent in the background.
+eval `ssh-agent -s`
+
+# Download Bitbucket Read-Only Access Key from presigned S3-url 
+curl -o /home/ubuntu/.ssh/repo_rsa $keyurl
+# Change permission of repo_key to restrict access
+chmod 400 /home/ubuntu/.ssh/repo_rsa
+# Load repo key to ssh-agent
+ssh-add /home/ubuntu/.ssh/repo_rsa
+
+# Set up project repo.
+# Pull from git repository or docker image registry.
+cd $MYGO
 git clone git@bitbucket.org:gainsresearch/go.git .
 git co -b gostructure
 
 chown -R ubuntu:staff $GOPATH
 go get github.com/lib/pq
+
 reboot 0
